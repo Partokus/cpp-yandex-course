eb5b24824daadcbc808e0a2fdc504b7dbc3ac1ad
Alexey Lepeshkin <a.lepeshkin@1440.space>
2023-07-27 20:32:33 +0300

    progress

--- src/main.cpp
+++ src/main.cpp
@@ -15,69 +15,238 @@
 
 using namespace std;
 
-template <typename T>
-T Sqr(T obj);
-
-template <typename T>
-vector<T> operator*(const vector<T> &lhs, const vector<T> &rhs);
-
-template <typename First, typename Second>
-pair<First, Second> operator*(const pair<First, Second> &lhs, const pair<First, Second> &rhs);
-
-template <typename Key, typename Value>
-map<Key, Value> operator*(const map<Key, Value> &lhs, const map<Key, Value> &rhs);
-
-template <typename T>
-T Sqr(T obj)
+enum class QueryType
 {
-    return obj * obj;
-}
+    NewBus,
+    BusesForStop,
+    StopsForBus,
+    AllBuses
+};
 
-template <typename T>
-vector<T> operator*(const vector<T> &lhs, const vector<T> &rhs)
+struct Query
 {
-    vector<T> result(lhs.size());
-    transform(lhs.cbegin(), lhs.cend(), result.begin(), [](const T &i)
-              { return i * i; });
-    return result;
-}
+    QueryType type;
+    string bus;
+    string stop;
+    vector<string> stops;
+};
 
-template <typename First, typename Second>
-pair<First, Second> operator*(const pair<First, Second> &lhs, const pair<First, Second> &rhs)
+istream &operator>>(istream &is, Query &q)
 {
-    return {lhs.first * lhs.first, lhs.second * lhs.second};
-}
-
-template <typename Key, typename Value>
-map<Key, Value> operator*(const map<Key, Value> &lhs, const map<Key, Value> &rhs)
-{
-    map<Key, Value> result = lhs;
-    for (auto &[key, value] : result)
+    string query;
+    is >> query;
+    if (query == "NEW_BUS")
     {
-        value = value * value;
+        q.type = QueryType::NewBus;
+
+        is >> q.bus;
+        int stop_count;
+        is >> stop_count;
+        q.stops.resize(stop_count);
+        for (string &stop : q.stops)
+        {
+            is >> stop;
+        }
     }
-    return result;
+    else if (query == "BUSES_FOR_STOP")
+    {
+        q.type = QueryType::BusesForStop;
+
+        is >> q.stop;
+    }
+    else if (query == "STOPS_FOR_BUS")
+    {
+        q.type = QueryType::StopsForBus;
+
+
+    }
+    else if (query == "ALL_BUSES")
+    {
+        q.type = QueryType::AllBuses;
+
+
+    }
+    return is;
 }
+
+struct BusesForStopResponse
+{
+    vector<string> buses = {};
+};
+
+ostream &operator<<(ostream &os, const BusesForStopResponse &r)
+{
+    if (r.buses.empty())
+    {
+        os << "No stop" << endl;
+    }
+    else
+    {
+        for (const string &bus : r.buses)
+        {
+            os << bus << " ";
+        }
+        os << endl;
+    }
+    return os;
+}
+
+struct StopsForBusResponse
+{
+    // Наполните полями эту структуру
+};
+
+ostream &operator<<(ostream &os, const StopsForBusResponse &r)
+{
+    // Реализуйте эту функцию
+    return os;
+}
+
+struct AllBusesResponse
+{
+    // Наполните полями эту структуру
+};
+
+ostream &operator<<(ostream &os, const AllBusesResponse &r)
+{
+    // Реализуйте эту функцию
+    return os;
+}
+
+class BusManager
+{
+public:
+    void AddBus(const string &bus, const vector<string> &stops)
+    {
+        for (const string &stop : stops)
+        {
+            stops_to_buses[stop].push_back(bus);
+        }
+    }
+
+    BusesForStopResponse GetBusesForStop(const string &stop) const
+    {
+        if (stops_to_buses.count(stop) == 0)
+        {
+            return {};
+        }
+        else
+        {
+            return {.buses = stops_to_buses.at(stop)};
+        }
+    }
+
+    StopsForBusResponse GetStopsForBus(const string &bus) const
+    {
+        // Реализуйте этот метод
+    }
+
+    AllBusesResponse GetAllBuses() const
+    {
+        // Реализуйте этот метод
+    }
+
+private:
+    map<string, vector<string>> buses_to_stops;
+    map<string, vector<string>> stops_to_buses;
+};
 
 int main()
 {
-    // vector<vector<int>> v = {{1, 2, 3}, {4, 5, 6}};
-    // cout << "vector:";
-    // for (const auto &x : Sqr(v))
-    // {
-    //     for (const auto &y : x)
-    //     {
-    //         cout << ' ' << y;
-    //     }
-    // }
+    int q;
+    cin >> q;
 
-    map<int, pair<int, int>> map_of_pairs = {
-        {4, {2, 2}},
-        {7, {4, 3}}};
-    cout << "map of pairs:" << endl;
-    for (const auto &x : Sqr(map_of_pairs))
+    map<string, vector<string>> buses_to_stops, stops_to_buses;
+
+    for (int i = 0; i < q; ++i)
     {
-        cout << x.first << ' ' << x.second.first << ' ' << x.second.second << endl;
+        string operation_code;
+        cin >> operation_code;
+
+        if (operation_code == "NEW_BUS")
+        {
+
+            string bus;
+            cin >> bus;
+            int stop_count;
+            cin >> stop_count;
+            vector<string> &stops = buses_to_stops[bus];
+            stops.resize(stop_count);
+            for (string &stop : stops)
+            {
+                cin >> stop;
+                stops_to_buses[stop].push_back(bus);
+            }
+        }
+        else if (operation_code == "BUSES_FOR_STOP")
+        {
+            string stop;
+            cin >> stop;
+            if (stops_to_buses.count(stop) == 0)
+            {
+                cout << "No stop" << endl;
+            }
+            else
+            {
+                for (const string &bus : stops_to_buses[stop])
+                {
+                    cout << bus << " ";
+                }
+                cout << endl;
+            }
+        }
+        else if (operation_code == "STOPS_FOR_BUS")
+        {
+
+            string bus;
+            cin >> bus;
+            if (buses_to_stops.count(bus) == 0)
+            {
+                cout << "No bus" << endl;
+            }
+            else
+            {
+                for (const string &stop : buses_to_stops[bus])
+                {
+                    cout << "Stop " << stop << ": ";
+                    if (stops_to_buses[stop].size() == 1)
+                    {
+                        cout << "no interchange";
+                    }
+                    else
+                    {
+                        for (const string &other_bus : stops_to_buses[stop])
+                        {
+                            if (bus != other_bus)
+                            {
+                                cout << other_bus << " ";
+                            }
+                        }
+                    }
+                    cout << endl;
+                }
+            }
+        }
+        else if (operation_code == "ALL_BUSES")
+        {
+
+            if (buses_to_stops.empty())
+            {
+                cout << "No buses" << endl;
+            }
+            else
+            {
+                for (const auto &bus_item : buses_to_stops)
+                {
+                    cout << "Bus " << bus_item.first << ": ";
+                    for (const string &stop : bus_item.second)
+                    {
+                        cout << stop << " ";
+                    }
+                    cout << endl;
+                }
+            }
+        }
     }
     return 0;
 }
