a0501d33cc65da7faa710a669650ce005fcf7b5c
Alexey Lepeshkin <a.lepeshkin@1440.space>
2023-09-18 15:10:39 +0300

    chrono personal budjet

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,124 @@
 #include <test_runner.h>
 #include <algorithm>
+#include <numeric>
+#include <cmath>
+#include <optional>
+#include <tuple>
+#include <ctime>
+#include <cstring>
 
 using namespace std;
 
-vector<string> SplitIntoWords(const string& s)
+class PersonalBudjet
 {
-    auto it_first_word_symbol = s.begin();
-    auto it_space = find(it_first_word_symbol, s.end(), ' ');
-    vector<string> result = {string{it_first_word_symbol, it_space}};
-
-    while (it_space != s.end())
+public:
+    double ComputeIncome(const std::chrono::year year_from, const std::chrono::month month_from, const std::chrono::day day_from,
+                         const std::chrono::year year_to, const std::chrono::month month_to, const std::chrono::day day_to) const
     {
-        it_first_word_symbol = it_space + 1;
-        it_space = find(it_first_word_symbol, s.end(), ' ');
-        result.push_back(string{it_first_word_symbol, it_space});
+        const auto date_begin = MakeTimePoint(year_from, month_from, day_from);
+        const auto date_end = MakeTimePoint(year_to, month_to, day_to) + std::chrono::days{1};
+        return accumulate(_dates.lower_bound(date_begin), _dates.find(date_end), 0.0, [](double value, const auto &my_pair)
+                          { return my_pair.second + value; });
     }
-    return result;
-}
 
-void TestSplitIntoWords()
-{
-    AssertEqual(SplitIntoWords("makaka banan dengi lave"), vector<string>{"makaka", "banan", "dengi", "lave"});
-    AssertEqual(SplitIntoWords("bogatstvo clubi resorani buhlo"), vector<string>{"bogatstvo", "clubi", "resorani", "buhlo"});
-    AssertEqual(SplitIntoWords("bogatstvo"), vector<string>{"bogatstvo"});
-    AssertEqual(SplitIntoWords("b"), vector<string>{"b"});
-    AssertEqual(SplitIntoWords("b a c"), vector<string>{"b", "a", "c"});
-}
+    void Earn(const std::chrono::year year_from, const std::chrono::month month_from, const std::chrono::day day_from,
+              const std::chrono::year year_to, const std::chrono::month month_to, const std::chrono::day day_to,
+              double value)
+    {
+        auto date = MakeTimePoint(year_from, month_from, day_from);
+        const auto date_end = MakeTimePoint(year_to, month_to, day_to) + std::chrono::days{1};
+
+        const auto days_count = std::chrono::duration_cast<std::chrono::days>(date_end - date).count();
+        const double value_for_day = value / days_count;
+
+        while (date != date_end)
+        {
+            _dates[date] += value_for_day;
+            date += std::chrono::days{1};
+        }
+    }
+
+private:
+    map<time_t, double> _dates{};
+
+    std::chrono::system_clock::time_point MakeTimePoint(std::chrono::year year,
+                                                        std::chrono::month month,
+                                                        std::chrono::day day) const
+    {
+        const std::chrono::year_month_day ymd{year, month, day};
+        return std::chrono::sys_days{ymd};
+    }
+};
 
 void TestAll()
 {
     TestRunner tr = {};
-    tr.RunTest(TestSplitIntoWords, "TestSplitIntoWords");
+    // tr.RunTest(TestOperatorDateLess, "TestOperatorDateLess");
 }
 
-// int main()
-// {
-//     TestAll();
+int main()
+{
+    TestAll();
 
-//     string s = "C Cpp Java Python";
+    // using namespace std::chrono;
 
-//     vector<string> words = SplitIntoWords(s);
-//     cout << words.size() << " ";
-//     for (auto it = begin(words); it != end(words); ++it)
-//     {
-//         if (it != begin(words))
-//         {
-//             cout << "/";
-//         }
-//         cout << *it;
-//     }
-//     cout << endl;
-//     return 0;
-// }
+    // cout << fixed << setprecision(25);
+
+    // PersonalBudjet pb{};
+    // int Q = 0;
+    // cin >> Q;
+
+    // for (int i = 0; i < Q; ++i)
+    // {
+    //     string req{};
+    //     cin >> req;
+
+    //     int year_from = 0;
+    //     unsigned int month_from = 0;
+    //     unsigned int day_from = 0;
+    //     int year_to = 0;
+    //     unsigned int month_to = 0;
+    //     unsigned int day_to = 0;
+
+    //     char separ{};
+
+    //     cin >> year_from >> separ >> month_from >> separ >> day_from >>
+    //         year_to >> separ >> month_to >> separ >> day_to;
+
+    //     if (req == "Earn")
+    //     {
+    //         unsigned int value = 0;
+    //         cin >> value;
+    //         pb.Earn(year{year_from}, month{month_from}, day{day_from},
+    //                 year{year_to}, month{month_to}, day{day_to},
+    //                 value);
+    //     }
+    //     else if (req == "ComputeIncome")
+    //     {
+    //         cout << pb.ComputeIncome(year{year_from}, month{month_from}, day{day_from},
+    //                                  year{year_to}, month{month_to}, day{day_to}) << endl;
+    //     }
+    // }
+
+    // std::time_t result = std::time(nullptr);
+    // std::cout << std::ctime(&result);
+
+    // char buffer[32];
+    // std::strncpy(buffer, std::ctime(&result), 26);
+    // assert('\n' == buffer[std::strlen(buffer) - 1]);
+    // std::cout << buffer;
+    time_t now = time(0);
+
+    cout << "Number of sec since January 1,1970 is:: " << now << endl;
+
+    tm *ltm = localtime(&now);
+
+    // print various components of tm structure.
+    cout << "Year:" << 1900 + ltm->tm_year << endl;
+    cout << "Month: " << 1 + ltm->tm_mon << endl;
+    cout << "Day: " << ltm->tm_mday << endl;
+    cout << "Time: " << 5 + ltm->tm_hour << ":";
+    cout << 30 + ltm->tm_min << ":";
+    cout << ltm->tm_sec << endl;
+    return 0;
+}
