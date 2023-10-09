d878c8a5adb3c533f4e09e35d36f71e7d5bac99f
Alexey Lepeshkin <a.lepeshkin@1440.space>
2023-10-09 15:03:54 +0300

    first and last names 4 done

--- src/main.cpp
+++ src/main.cpp
@@ -7,162 +7,95 @@
 #include <ctime>
 #include <cstring>
 #include <iomanip>
-#include <span>
 
 using namespace std;
 
-struct Date
-{
-    int year = 0;
-    int month = 0;
-    int day = 0;
-};
-
-bool operator<(const Date &lhs, const Date &rhs)
-{
-    return make_tuple(lhs.year, lhs.month, lhs.day) < make_tuple(rhs.year, rhs.month, rhs.day);
-}
-
-template <class InputIt, class OutputKey, class OutputValue, class BinaryOperator>
-void MapPartialSum(InputIt first, InputIt last,
-                   map<OutputKey, OutputValue> &dest,
-                   BinaryOperator op)
-{
-    if (first == last)
-        return;
-
-    typename InputIt::value_type::second_type sum = first->second;
-    dest[first->first] = sum;
-
-    while (++first != last)
-    {
-        sum = op(sum, first->second);
-        dest[first->first] = sum;
-    }
-}
-
-template <class InputIt, class OutputKey, class OutputValue>
-void MapPartialSum(InputIt first, InputIt last,
-                   map<OutputKey, OutputValue> &dest)
-{
-    return MapPartialSum(first, last, dest, std::plus<OutputValue>());
-}
-
-class PersonalBudjet
+class Person
 {
 public:
-    double ComputeIncome(Date date_from, Date date_to)
+    void ChangeFirstName(int year, const string &first_name)
     {
-        if (date_to < date_from)
-        {
-            swap(date_from, date_to);
-        }
-        const time_t from = MakeTime(date_from);
-        const time_t to = MakeTime(date_to);
-
-        auto it_from = _budjet.find(from);
-        if (it_from == _budjet.end())
-        {
-            it_from = _budjet.upper_bound(from);
-        }
-
-        auto it_to = _budjet.find(to);
-        if (it_to == _budjet.end())
-        {
-            it_to = prev(_budjet.lower_bound(to));
-        }
-
-        if (not _isPartialSumUpdated)
-        {
-            _isPartialSumUpdated = true;
-            MapPartialSum(_budjet.cbegin(), _budjet.cend(), _budjet);
-        }
-
-        const double lhs_sum = it_from == _budjet.cbegin() ? 0.0 : prev(it_from)->second;
-        const double rhs_sum = it_to->second;
-
-        return rhs_sum - lhs_sum;
+        _first_names[year] = first_name;
     }
 
-    void Earn(const Date date, const double value)
+    void ChangeLastName(int year, const string &last_name)
     {
-        _budjet[MakeTime(date)] += value;
+        _last_names[year] = last_name;
+    }
+
+    string GetFullName(int year) const
+    {
+        auto first_name_year = findNearlyYear(year, _first_names);
+        auto last_name_year = findNearlyYear(year, _last_names);
+
+        if (not first_name_year and not last_name_year)
+        {
+            return "Incognito";
+        }
+        if (first_name_year and not last_name_year)
+        {
+            return _first_names.at(first_name_year.value()) + " with unknown last name";
+        }
+        else if (last_name_year and not first_name_year)
+        {
+            return _last_names.at(last_name_year.value()) + " with unknown first name";
+        }
+
+        return _first_names.at(first_name_year.value()) + " " + _last_names.at(last_name_year.value());
     }
 
 private:
-    map<time_t, double> _budjet{};
-    bool _isPartialSumUpdated = false;
+    map<int, string> _first_names;
+    map<int, string> _last_names;
 
-    static constexpr time_t OneDay = 60 * 60 * 24;
-
-    time_t MakeTime(const Date &date) const
+    /**
+     * findNearlyYear
+     * * return bigger or same year if exists
+     */
+    std::optional<int> findNearlyYear(int year, const map<int, string> &names) const noexcept
     {
-        time_t empty_time{};
-        tm *ltm = localtime(&empty_time);
-        ltm->tm_year = date.year - 1900;
-        ltm->tm_mon = date.month - 1;
-        ltm->tm_mday = date.day;
-        return mktime(ltm);
-    }
-
-    unsigned int CountDays(time_t date_time_from, time_t date_time_to)
-    {
-        return ((date_time_to - date_time_from) / OneDay) + 1;
+        auto iter_after = names.upper_bound(year);
+        if (iter_after != names.begin())
+        {
+            return prev(iter_after)->first;
+        }
+        return std::nullopt;
     }
 };
-
-void TestPersonalBudjet()
-{
-}
 
 void TestAll()
 {
     TestRunner tr = {};
-    tr.RunTest(TestPersonalBudjet, "TestPersonalBudjet");
 }
 
 int main()
 {
-    cout.precision(25);
-
     TestAll();
 
-    PersonalBudjet pb{};
+    Person person;
 
-    int E = 0;
-    cin >> E;
-
-    // E раз добавляем информацию о заработках, например:
-    // 2000-01-01 10
-    for (int i = 0; i < E; ++i)
+    for (int year : {1900, 1965, 1990})
     {
-        Date date{};
-        double value = 0.0;
-
-        char separ{};
-
-        cin >> date.year >> separ >> date.month >> separ >> date.day >> value;
-        pb.Earn(date, value);
+        cout << person.GetFullName(year) << endl;
     }
 
-    int Q = 0;
-    cin >> Q;
-
-    map<time_t, double> sums{};
-
-    // Q раз вычисляем прибыль, например:
-    // 2000-01-01 2000-01-06
-    for (int i = 0; i < Q; ++i)
+    person.ChangeFirstName(1965, "Polina");
+    person.ChangeLastName(1967, "Sergeeva");
+    for (int year : {1900, 1965, 1990})
     {
-        Date date_from{};
-        Date date_to{};
+        cout << person.GetFullName(year) << endl;
+    }
 
-        char separ{};
+    person.ChangeFirstName(1970, "Appolinaria");
+    for (int year : {1969, 1970})
+    {
+        cout << person.GetFullName(year) << endl;
+    }
 
-        cin >> date_from.year >> separ >> date_from.month >> separ >> date_from.day >>
-            date_to.year >> separ >> date_to.month >> separ >> date_to.day;
-
-        cout << pb.ComputeIncome(date_from, date_to) << endl;
+    person.ChangeLastName(1968, "Volkova");
+    for (int year : {1969, 1970})
+    {
+        cout << person.GetFullName(year) << endl;
     }
     return 0;
 }
