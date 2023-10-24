e49cbb9aa9c67c1dd9c1fc7386fa81c8acd960d2
Alexey Lepeshkin <a.lepeshkin@1440.space>
2023-10-24 12:00:07 +0300

    figures

--- src/main.cpp
+++ src/main.cpp
@@ -7,59 +7,180 @@
 #include <ctime>
 #include <cstring>
 #include <iomanip>
+#include <string_view>
+#include <memory>
 
 using namespace std;
 
-template <typename RandomIt>
-pair<RandomIt, RandomIt> FindStartsWith(RandomIt range_begin, RandomIt range_end, const string &prefix)
+class Figure
 {
-    // Все строки, начинающиеся с prefix, больше или равны строке "<prefix>"
-    auto left = lower_bound(range_begin, range_end, prefix);
+public:
+    virtual ~Figure() = default;
 
-    // Составим строку, которая в рамках буквенных строк является
-    // точной верхней гранью множества строк, начинающихся с prefix
-    string upper_bound = prefix;
-    ++upper_bound[upper_bound.size() - 1];
+    virtual string Name() const = 0;
+    virtual double Perimeter() const = 0;
+    virtual double Area() const = 0;
+};
 
-    // Первое встреченное слово, не меньшее upper_bound,
-    // обязательно является концом полуинтервала
-    auto right = lower_bound(range_begin, range_end, upper_bound);
+class Triangle : public Figure
+{
+public:
+    Triangle(unsigned int a, unsigned int b, unsigned int c)
+    {
+        _perimeter = a + b + c;
+        double half_p = _perimeter / 2.0;
+        _area = sqrt(half_p * (half_p - a) * (half_p - b) * (half_p - c));
+    }
+    ~Triangle() override = default;
 
-    return {left, right};
+    string Name() const override { return NameVar; }
+    double Perimeter() const override { return _perimeter; }
+    double Area() const override { return _area; }
+
+private:
+    const string NameVar = "TRIANGLE";
+    double _perimeter = 0.0;
+    double _area = 0.0;
+};
+
+class Rect : public Figure
+{
+public:
+    Rect(unsigned int a, unsigned int b)
+    {
+        _perimeter = 2 * (a + b);
+        _area = a * b;
+    }
+    ~Rect() override = default;
+
+    string Name() const override { return NameVar; }
+    double Perimeter() const override { return _perimeter; }
+    double Area() const override { return _area; }
+
+private:
+    const string NameVar = "RECT";
+    double _perimeter = 0.0;
+    double _area = 0.0;
+};
+
+class Circle : public Figure
+{
+public:
+    Circle(double radius)
+    {
+        static constexpr double Pi = 3.14;
+        _perimeter = 2.0 * Pi * radius;
+        _area = Pi * radius * radius;
+    }
+    ~Circle() override = default;
+
+    string Name() const override { return NameVar; }
+    double Perimeter() const override { return _perimeter; }
+    double Area() const override { return _area; }
+
+private:
+    const string NameVar = "CIRCLE";
+    double _perimeter = 0.0;
+    double _area = 0.0;
+};
+
+shared_ptr<Figure> CreateFigure(istringstream &is)
+{
+    shared_ptr<Figure> result{};
+
+    string figure{};
+    is >> figure;
+
+    if (figure == "TRIANGLE")
+    {
+        unsigned int a = 0;
+        unsigned int b = 0;
+        unsigned int c = 0;
+
+        is >> a >> b >> c;
+
+        result = make_shared<Triangle>(a, b, c);
+    }
+    else if (figure == "RECT")
+    {
+        unsigned int a = 0;
+        unsigned int b = 0;
+
+        is >> a >> b;
+
+        result = make_shared<Rect>(a, b);
+    }
+    else if (figure == "CIRCLE")
+    {
+        double radius = 0.0;
+
+        is >> radius;
+
+        result = make_shared<Circle>(radius);
+    }
+    return result;
 }
 
-void TestFindStartsWith()
+void TestTriangle()
 {
+    {
+        Triangle t(5, 4, 7);
+        AssertEqual(t.Perimeter(), 16U, "Triangle 0");
+        Assert(9.79 <= t.Area() and t.Area() <= 9.81, "Triangle 1");
+    }
+}
+
+void TestRect()
+{
+    {
+        Rect r(5, 4);
+        AssertEqual(r.Perimeter(), 18U, "Rect 0");
+        Assert(19.9 <= r.Area() and r.Area() <= 20.1, "Rect 1");
+    }
+}
+
+void TestCircle()
+{
+    {
+        Circle c(5.0);
+        Assert(31.3 <= c.Perimeter() and c.Perimeter() <= 31.5, "Circle 0");
+        Assert(78.4 <= c.Area() and c.Area() <= 78.6, "Circle 1");
+    }
 }
 
 void TestAll()
 {
-    TestRunner tr = {};
-
-    tr.RunTest(TestFindStartsWith, "TestFindStartsWith");
+    TestRunner tr{};
+    tr.RunTest(TestTriangle, "TestTriangle");
+    tr.RunTest(TestRect, "TestRect");
+    tr.RunTest(TestCircle, "TestCircle");
 }
 
 int main()
 {
     TestAll();
 
-    const vector<string> sorted_strings = {"moscow", "motovilikha", "murmansk", "m", "p"};
-
-    const auto mo_result =
-        FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
-    for (auto it = mo_result.first; it != mo_result.second; ++it)
+    vector<shared_ptr<Figure>> figures;
+    for (string line; getline(cin, line);)
     {
-        cout << *it << " ";
+        istringstream is(line);
+
+        string command;
+        is >> command;
+        if (command == "ADD")
+        {
+            figures.push_back(CreateFigure(is));
+        }
+        else if (command == "PRINT")
+        {
+            for (const auto &current_figure : figures)
+            {
+                cout << fixed << setprecision(3)
+                     << current_figure->Name() << " "
+                     << current_figure->Perimeter() << " "
+                     << current_figure->Area() << endl;
+            }
+        }
     }
-    cout << endl;
-
-    const auto mt_result =
-        FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
-    cout << (mt_result.first - begin(sorted_strings)) << " " << (mt_result.second - begin(sorted_strings)) << endl;
-
-    const auto na_result =
-        FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
-    cout << (na_result.first - begin(sorted_strings)) << " " << (na_result.second - begin(sorted_strings)) << endl;
-
     return 0;
 }
