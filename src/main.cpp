2bb3d96f71314172b69fbc82a96a46c2a1a16a86
Alexey Lepeshkin <a.lepeshkin@1440.space>
2023-11-30 11:13:53 +0300

    bad macros

--- src/main.cpp
+++ src/main.cpp
@@ -12,14 +12,24 @@ void TestAll();
 
 using namespace std;
 
+#define PRINT_VALUES(out, x, y) ((out) << (x) << endl << (y) << endl)
+
 int main()
 {
     TestAll();
+
     return 0;
+}
+
+void TestPrintValue()
+{
+    ostringstream output;
+    PRINT_VALUES(output, 5, "red belt");
+    ASSERT_EQUAL(output.str(), "5\nred belt\n");
 }
 
 void TestAll()
 {
     TestRunner tr;
-    // RUN_TEST(tr, TestUniqueMax);
+    RUN_TEST(tr, TestPrintValue);
 }
