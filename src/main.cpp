a3d58480a0bf953b6b48ccb1661652f99bdab637
Alexey Lepeshkin <a.lepeshkin@1440.space>
2024-03-25 13:30:58 +0300

    sorted_words

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include <iomanip>
 #include <iostream>
 #include <set>
+#include <vector>
 #include <map>
 #include <string>
 #include <algorithm>
@@ -18,35 +19,72 @@ using namespace std;
 void TestAll();
 void Profile();
 
-template <typename RandomIt>
-void MakeJosephusPermutation(RandomIt first, RandomIt last, uint32_t step_size)
+// Объявляем Group<String> для произвольного типа String
+// синонимом vector<String>.
+// Благодаря этому в качестве возвращаемого значения
+// функции можно указать не малопонятный вектор векторов,
+// а вектор групп — vector<Group<String>>.
+template <typename String>
+using Group = vector<String>;
+
+// Ещё один шаблонный синоним типа
+// позволяет вместо громоздкого typename String::value_type
+// использовать Char<String>
+template <typename String>
+using Char = typename String::value_type;
+
+template <typename String>
+vector<Group<String>> GroupHeavyStrings(vector<String> strings)
 {
-    list<typename RandomIt::value_type> pool(make_move_iterator(first), make_move_iterator(last));
-
-    auto cur_pos_it = pool.begin();
-    size_t cur_pos = 0;
-    while (!pool.empty())
+    if (strings.empty())
     {
-        *(first++) = move(*cur_pos_it);
-        cur_pos_it = pool.erase(cur_pos_it);
-        if (pool.empty())
-        {
-            break;
-        }
-
-        size_t last_pos = cur_pos;
-        cur_pos = (cur_pos + step_size - 1) % pool.size();
-
-        if (last_pos > cur_pos)
-        {
-            cur_pos_it = pool.begin();
-            advance(cur_pos_it, cur_pos);
-        }
-        else
-        {
-            advance(cur_pos_it, cur_pos - last_pos);
-        }
+        return {};
     }
+    if (strings.size() == 1U)
+    {
+        return {Group<String>{strings.front()}};
+    }
+
+    vector<Group<String>> result;
+    result.reserve(strings.size());
+
+    list<set<Char<String>>> sorted_chars(strings.size());
+
+    auto it = sorted_chars.begin();
+    for (const auto &word : strings)
+    {
+        *it++ = set<Char<String>>{word.cbegin(), word.cend()};
+    }
+
+    list<size_t> strings_indexes(strings.size());
+    iota(strings_indexes.begin(), strings_indexes.end(), 0U);
+
+    while (not sorted_chars.empty())
+    {
+        auto &leader_word = sorted_chars.front();
+        result.resize(result.size() + 1U);
+        result.back().push_back(move(strings[strings_indexes.front()]));
+        auto it_strings_index = strings_indexes.erase(strings_indexes.begin());
+
+        for (auto it_sorted_word = next(sorted_chars.begin()); it_sorted_word != sorted_chars.cend();)
+        {
+            if (leader_word == *it_sorted_word)
+            {
+                result.back().push_back(move(strings[*it_strings_index]));
+                it_strings_index = strings_indexes.erase(it_strings_index);
+                it_sorted_word = sorted_chars.erase(it_sorted_word);
+            }
+            else
+            {
+                ++it_sorted_word;
+                ++it_strings_index;
+            }
+        }
+        sorted_chars.erase(sorted_chars.begin());
+    }
+
+    result.shrink_to_fit();
+    return result;
 }
 
 int main()
@@ -56,108 +94,35 @@ int main()
     return 0;
 }
 
-// Это специальный тип, который поможет вам убедиться, что ваша реализация
-// функции MakeJosephusPermutation не выполняет копирование объектов.
-// Сейчас вы, возможно, не понимаете как он устроен, однако мы расскажем,
-// почему он устроен именно так, далее в блоке про move-семантику —
-// в видео «Некопируемые типы»
-struct NoncopyableInt
+void TestGroupingABC()
 {
-    int value;
-
-    NoncopyableInt(const NoncopyableInt &) = delete;
-    NoncopyableInt &operator=(const NoncopyableInt &) = delete;
-
-    NoncopyableInt(NoncopyableInt &&) = default;
-    NoncopyableInt &operator=(NoncopyableInt &&) = default;
-};
-
-vector<int> MakeTestVector()
-{
-    vector<int> numbers(10);
-    iota(begin(numbers), end(numbers), 0);
-    return numbers;
+    vector<string> strings = {"caab", "abc", "cccc", "bacc", "c"};
+    auto groups = GroupHeavyStrings(strings);
+    ASSERT_EQUAL(groups.size(), 2);
+    sort(begin(groups), end(groups)); // Порядок групп не имеет значения
+    ASSERT_EQUAL(groups[0], vector<string>({"caab", "abc", "bacc"}));
+    ASSERT_EQUAL(groups[1], vector<string>({"cccc", "c"}));
 }
 
-void TestIntVector()
+void TestGroupingReal()
 {
-    const vector<int> numbers = MakeTestVector();
-    {
-        vector<int> numbers_copy = numbers;
-        MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 1);
-        ASSERT_EQUAL(numbers_copy, numbers);
-    }
-    {
-        vector<int> numbers_copy = numbers;
-        MakeJosephusPermutation(begin(numbers_copy), end(numbers_copy), 3);
-        ASSERT_EQUAL(numbers_copy, vector<int>({0, 3, 6, 9, 4, 8, 5, 2, 7, 1}));
-    }
-}
-
-bool operator==(const NoncopyableInt &lhs, const NoncopyableInt &rhs)
-{
-    return lhs.value == rhs.value;
-}
-
-ostream &operator<<(ostream &os, const NoncopyableInt &v)
-{
-    return os << v.value;
-}
-
-void TestAvoidsCopying()
-{
-    vector<NoncopyableInt> numbers;
-    numbers.push_back({1});
-    numbers.push_back({2});
-    numbers.push_back({3});
-    numbers.push_back({4});
-    numbers.push_back({5});
-
-    MakeJosephusPermutation(begin(numbers), end(numbers), 2);
-
-    vector<NoncopyableInt> expected;
-    expected.push_back({1});
-    expected.push_back({3});
-    expected.push_back({5});
-    expected.push_back({4});
-    expected.push_back({2});
-
-    ASSERT_EQUAL(numbers, expected);
+    vector<string> strings = {"law", "port", "top", "laptop", "pot", "paloalto", "wall", "awl"};
+    auto groups = GroupHeavyStrings(strings);
+    ASSERT_EQUAL(groups.size(), 4);
+    sort(begin(groups), end(groups)); // Порядок групп не имеет значения
+    ASSERT_EQUAL(groups[0], vector<string>({"laptop", "paloalto"}));
+    ASSERT_EQUAL(groups[1], vector<string>({"law", "wall", "awl"}));
+    ASSERT_EQUAL(groups[2], vector<string>({"port"}));
+    ASSERT_EQUAL(groups[3], vector<string>({"top", "pot"}));
 }
 
 void TestAll()
 {
     TestRunner tr{};
-    RUN_TEST(tr, TestIntVector);
-    RUN_TEST(tr, TestAvoidsCopying);
+    RUN_TEST(tr, TestGroupingABC);
+    RUN_TEST(tr, TestGroupingReal);
 }
 
 void Profile()
 {
-    // const size_t Size = 1000'000;
-    // const string s = "hello";
-    // {
-    //     deque<string> strings;
-
-    //     {
-    //         strings.assign(Size, s);
-
-    //         LOG_DURATION("deque pop_front");
-
-    //         while (not strings.empty())
-    //         {
-    //             strings.pop_front();
-    //         }
-    //     }
-    //     {
-    //         strings.assign(Size, s);
-
-    //         LOG_DURATION("deque erase");
-
-    //         while (not strings.empty())
-    //         {
-    //             strings.erase(strings.begin());
-    //         }
-    //     }
-    // }
 }
