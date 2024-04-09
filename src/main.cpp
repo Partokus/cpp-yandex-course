563b07460744427e7e7c79b812326ad416c6b669
Alexey Lepeshkin <a.lepeshkin@1440.space>
2024-04-09 12:00:37 +0300

    simple vector copy assignment ctor

--- src/main.cpp
+++ src/main.cpp
@@ -1,49 +1,18 @@
+#include "simple_vector.h"
 #include <profile.h>
+#include <iostream>
 #include <test_runner.h>
 #include <vector>
 #include <string>
 #include <algorithm>
 #include <iterator>
+#include <numeric>
+
 
 using namespace std;
 
 void TestAll();
 void Profile();
-
-// Объявляем Group<String> для произвольного типа String
-// синонимом vector<String>.
-// Благодаря этому в качестве возвращаемого значения
-// функции можно указать не малопонятный вектор векторов,
-// а вектор групп — vector<Group<String>>.
-template <typename String>
-using Group = vector<String>;
-
-// Ещё один шаблонный синоним типа
-// позволяет вместо громоздкого typename String::value_type
-// использовать Char<String>
-template <typename String>
-using Char = typename String::value_type;
-
-template <typename String>
-vector<Group<String>> GroupHeavyStrings(vector<String> strings)
-{
-    using Letters = set<Char<String>>;
-
-    map<Letters, Group<String>> groups;
-
-    for (String &str : strings)
-    {
-        groups[Letters{str.begin(), str.end()}].emplace_back(move(str));
-    }
-
-    vector<Group<String>> result;
-    result.reserve(strings.size());
-    for (auto &[letters, group] : groups)
-    {
-        result.emplace_back(move(group));
-    }
-    return result;
-}
 
 int main()
 {
@@ -52,90 +21,26 @@ int main()
     return 0;
 }
 
-void TestGroupingABC()
+void TestCopyAssignment()
 {
-    vector<string> strings = {"caab", "abc", "cccc", "bacc", "c"};
-    auto groups = GroupHeavyStrings(strings);
-    ASSERT_EQUAL(groups.size(), 2);
-    sort(begin(groups), end(groups)); // Порядок групп не имеет значения
-    ASSERT_EQUAL(groups[0], vector<string>({"caab", "abc", "bacc"}));
-    ASSERT_EQUAL(groups[1], vector<string>({"cccc", "c"}));
-}
+    SimpleVector<int> numbers(10);
+    iota(numbers.begin(), numbers.end(), 1);
 
-void TestGroupingReal()
-{
-    vector<string> strings = {"law", "port", "top", "laptop", "pot", "paloalto", "wall", "awl"};
-    auto groups = GroupHeavyStrings(strings);
-    ASSERT_EQUAL(groups.size(), 4);
-    sort(begin(groups), end(groups)); // Порядок групп не имеет значения
-    ASSERT_EQUAL(groups[0], vector<string>({"laptop", "paloalto"}));
-    ASSERT_EQUAL(groups[1], vector<string>({"law", "wall", "awl"}));
-    ASSERT_EQUAL(groups[2], vector<string>({"port"}));
-    ASSERT_EQUAL(groups[3], vector<string>({"top", "pot"}));
-}
+    SimpleVector<int> dest;
+    ASSERT_EQUAL(dest.Size(), 0u);
 
-void TestGroupingHeavy()
-{
-    vector<vector<string>> strings = {{"law", "wal", "wal", "law"}, {"lwa", "wal"}, {"wal", "law", "wal"}, {"top", "pot"}, {"pot", "top"}};
-    auto groups = GroupHeavyStrings(strings);
-    ASSERT_EQUAL(groups.size(), 3);
-    sort(begin(groups), end(groups)); // Порядок групп не имеет значения
-    ASSERT_EQUAL(groups[0], vector<vector<string>>({{"law", "wal", "wal", "law"}, {"wal", "law", "wal"}}));
-    ASSERT_EQUAL(groups[1], vector<vector<string>>({{"lwa", "wal"}}));
-    ASSERT_EQUAL(groups[2], vector<vector<string>>({{"top", "pot"}, {"pot", "top"}}));
+    dest = numbers;
+    ASSERT_EQUAL(dest.Size(), numbers.Size());
+    ASSERT(dest.Capacity() >= dest.Size());
+    ASSERT(equal(dest.begin(), dest.end(), numbers.begin()));
 }
 
 void TestAll()
 {
     TestRunner tr{};
-    RUN_TEST(tr, TestGroupingABC);
-    RUN_TEST(tr, TestGroupingReal);
-    RUN_TEST(tr, TestGroupingHeavy);
+    RUN_TEST(tr, TestCopyAssignment);
 }
 
 void Profile()
 {
-    string symbol1;
-    string symbol2;
-    string symbol3;
-    for (int i = 0; i < 10; ++i)
-    {
-        symbol1 += "ten symbo1";
-    }
-    for (int i = 0; i < 10; ++i)
-    {
-        symbol2 += "ten symbo3";
-    }
-    for (int i = 0; i < 10; ++i)
-    {
-        symbol3 += "ten symbo2";
-    }
-    using String = vector<string>;
-    vector<String> strings;
-
-    String str1;
-    for (int k = 0; k < 33; ++k)
-    {
-        str1.push_back(symbol1);
-    }
-    for (int k = 0; k < 33; ++k)
-    {
-        str1.push_back(symbol2);
-    }
-    for (int k = 0; k < 33; ++k)
-    {
-        str1.push_back(symbol3);
-    }
-
-    for (int i = 0; i < 30'000; ++i)
-    {
-        strings.push_back(str1);
-    }
-
-    {
-        LOG_DURATION("heavy");
-        auto groups = GroupHeavyStrings(strings);
-        size_t size = groups.size();
-        ++size;
-    }
 }
---
+++ src/simple_vector.h
@@ -0,0 +1,117 @@
+#include <algorithm>
+
+template <typename T>
+class SimpleVector
+{
+public:
+    SimpleVector() = default;
+    explicit SimpleVector(size_t size); // default ctor
+    explicit SimpleVector(const SimpleVector &other); // copy ctor
+    void operator=(const SimpleVector &other); // copy assignment ctor
+    ~SimpleVector();
+
+    T &operator[](size_t index);
+
+    T *begin();
+    T *end();
+    const T *begin() const;
+    const T *end() const;
+
+    size_t Size() const;
+    size_t Capacity() const;
+    void PushBack(const T &value);
+
+private:
+    T *_data = nullptr;
+    T *_end = nullptr;
+    size_t _size = 0U;
+    size_t _capacity = 0U;
+};
+
+template <typename T>
+SimpleVector<T>::SimpleVector(size_t size)
+    : _capacity(size)
+{
+    _data = new T[size];
+    _end = _data + size;
+    _size = size;
+    _capacity = size;
+}
+
+template <typename T>
+SimpleVector<T>::SimpleVector(const SimpleVector<T> &other)
+    : _data(new T[other.Capacity()]),
+      _size(other.Size()),
+      _capacity(other.Capacity())
+{
+    std::copy(other.cbegin(), other.cend(), begin());
+}
+
+template <typename T>
+void SimpleVector<T>::operator=(const SimpleVector<T> &other)
+{
+    if (_capacity == 0U)
+    {
+        _data = new T[other.Size()];
+        _capacity = other.Size();
+    }
+    else if (_capacity < other.Size())
+    {
+        this->SimpleVector<T>::~SimpleVector<T>();
+        _data = new T[other.Size()];
+        _capacity = other.Size();
+    }
+    std::copy(other.begin(), other.end(), begin());
+    _size = other.Size();
+    _end = _data + _size;
+}
+
+template <typename T>
+SimpleVector<T>::~SimpleVector()
+{
+    delete[] _data;
+}
+
+template <typename T>
+T *SimpleVector<T>::begin() { return _data; }
+template <typename T>
+T *SimpleVector<T>::end() { return _end; }
+template <typename T>
+const T *SimpleVector<T>::begin() const { return _data; }
+template <typename T>
+const T *SimpleVector<T>::end() const { return _end; }
+
+template <typename T>
+T &SimpleVector<T>::operator[](size_t index)
+{
+    return _data[index];
+}
+
+template <typename T>
+size_t SimpleVector<T>::Size() const { return _size; }
+template <typename T>
+size_t SimpleVector<T>::Capacity() const { return _capacity; }
+
+template <typename T>
+void SimpleVector<T>::PushBack(const T &value)
+{
+    if (_size == _capacity)
+    {
+        if (_capacity == 0U)
+        {
+            _capacity = 1U;
+            _data = new T[1U];
+        }
+        else
+        {
+            _capacity *= 2U;
+            T *new_data = new T[_capacity];
+            std::copy(_data, _end, new_data);
+            delete[] _data;
+            _data = new_data;
+        }
+    }
+    _data[_size] = value;
+    ++_size;
+    _end = _data + _size;
+}
