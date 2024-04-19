175e91abe17f79b6225f76e067733f489e5cae38
Alexey Lepeshkin <a.lepeshkin@1440.space>
2024-04-19 12:57:14 +0300

    save

--- src/main.cpp
+++ src/main.cpp
@@ -6,41 +6,128 @@
 #include <algorithm>
 #include <iterator>
 #include <numeric>
+#include <list>
+#include <utility>
+#include <functional>
 
 using namespace std;
 
 void TestAll();
 void Profile();
 
-template <typename RandomIt>
-void MergeSort(RandomIt range_begin, RandomIt range_end)
+template <typename T>
+class PriorityCollection
 {
-    auto elements_number = range_end - range_begin;
+public:
+    using Priority = size_t;
+    using Objects = list<T>;
+    using ObjectIterator = typename Objects::iterator;
+    using Id = ObjectIterator *;
 
-    if (elements_number < 2)
-    {
-        return;
-    }
+    // Добавить объект с нулевым приоритетом
+    // с помощью перемещения и вернуть его идентификатор
+    Id Add(T &&object);
 
-    vector<typename RandomIt::value_type> elements(make_move_iterator(range_begin), make_move_iterator(range_end));
+    // Добавить все элементы диапазона [range_begin, range_end)
+    // с помощью перемещения, записав выданные им идентификаторы
+    // в диапазон [ids_begin, ...)
+    template <typename ObjInputIt, typename IdOutputIt>
+    void Add(ObjInputIt range_begin, ObjInputIt range_end,
+             IdOutputIt ids_begin);
 
-    const size_t one_part_lenght = elements_number / 3;
-    auto begin_second_part = elements.begin() + one_part_lenght;
-    auto begin_third_part = begin_second_part + one_part_lenght;
+    // Определить, принадлежит ли идентификатор какому-либо
+    // хранящемуся в контейнере объекту
+    bool IsValid(Id id) const;
 
-    MergeSort(elements.begin(), begin_second_part);
-    MergeSort(begin_second_part, begin_third_part);
-    MergeSort(begin_third_part, elements.end());
+    // Получить объект по идентификатору
+    const T &Get(Id id) const;
 
-    vector<typename RandomIt::value_type> first_two_parts;
-    merge(make_move_iterator(elements.begin()), make_move_iterator(begin_second_part),
-          make_move_iterator(begin_second_part), make_move_iterator(begin_third_part),
-          back_inserter(first_two_parts));
+    // Увеличить приоритет объекта на 1
+    void Promote(Id id);
 
-    merge(make_move_iterator(first_two_parts.begin()), make_move_iterator(first_two_parts.end()),
-          make_move_iterator(begin_third_part), make_move_iterator(elements.end()),
-          range_begin);
+    // Получить объект с максимальным приоритетом и его приоритет
+    pair<const T &, int> GetMax() const;
+
+    // Аналогично GetMax, но удаляет элемент из контейнера
+    pair<T, int> PopMax();
+
+private:
+    map<Priority, Objects> _collection;
+    map<Id, Priority> _object_priority;
+};
+
+template <typename T>
+typename PriorityCollection<T>::Id PriorityCollection<T>::Add(T &&object)
+{
+    _collection[0].push_back(move(object));
+    ObjectIterator it_back = prev(_collection[0].end());
+    ObjectIterator it_back2 = prev(it_back);
+    Id ptr_on_it_back = &it_back2;
+    size_t ptr1 = reinterpret_cast<size_t>(ptr_on_it_back);
+    ptr_on_it_back = &it_back;
+    size_t ptr2 = reinterpret_cast<size_t>(ptr_on_it_back);
+    _object_priority[ptr_on_it_back] = 0U;
+    return ptr_on_it_back;
 }
+
+template <typename T>
+template <typename ObjInputIt, typename IdOutputIt>
+void PriorityCollection<T>::Add(ObjInputIt range_begin, ObjInputIt range_end,
+                                IdOutputIt ids_begin)
+{
+    for (auto it = range_begin; it != range_end; ++it)
+    {
+        *ids_begin++ = Add(*it);
+    }
+}
+
+template <typename T>
+bool PriorityCollection<T>::IsValid(Id id) const
+{
+    return _object_priority.count(id);
+}
+
+template <typename T>
+const T &PriorityCollection<T>::Get(Id id) const
+{
+    const ObjectIterator it = *id;
+    return *it;
+}
+
+template <typename T>
+void PriorityCollection<T>::Promote(Id id)
+{
+    const Priority priority = _object_priority[id];
+    const Priority next_priority = priority + 1U;
+    const ObjectIterator it = *id;
+    _collection[next_priority].push_back(move(*it));
+    _collection[priority].erase(it);
+}
+
+template <typename T>
+pair<const T &, int> PriorityCollection<T>::GetMax() const
+{
+    const auto &[priority, objects] = _collection.back();
+    return {objects.back(), priority};
+}
+
+template <typename T>
+pair<T, int> PriorityCollection<T>::PopMax()
+{
+    auto it_collection_back = prev(_collection.end());
+    auto &[priority, objects] = *it_collection_back;
+    T obj = move(objects.back());
+    objects.pop_back();
+    if (objects.empty())
+    {
+        _collection.erase(it_collection_back);
+    }
+    return {move(obj), priority};
+}
+
+
+
+
 
 int main()
 {
@@ -49,19 +136,71 @@ int main()
     return 0;
 }
 
-void TestIntVector()
+class StringNonCopyable : public string
 {
-    vector<int> numbers = {6, 1, 3, 9, 1, 9, 8, 12, 1};
-    MergeSort(begin(numbers), end(numbers));
-    ASSERT(is_sorted(begin(numbers), end(numbers)));
+public:
+    using string::string; // Позволяет использовать конструкторы строки
+    StringNonCopyable(const StringNonCopyable &) = delete;
+    StringNonCopyable(StringNonCopyable &&) = default;
+    StringNonCopyable &operator=(const StringNonCopyable &) = delete;
+    StringNonCopyable &operator=(StringNonCopyable &&) = default;
+};
+
+void TestNoCopy()
+{
+    PriorityCollection<StringNonCopyable> strings;
+    const auto white_id = strings.Add("white");
+    const auto yellow_id = strings.Add("yellow");
+    const auto red_id = strings.Add("red");
+
+    strings.Promote(yellow_id);
+    for (int i = 0; i < 2; ++i)
+    {
+        strings.Promote(red_id);
+    }
+    strings.Promote(yellow_id);
+    {
+        const auto item = strings.PopMax();
+        ASSERT_EQUAL(item.first, "red");
+        ASSERT_EQUAL(item.second, 2);
+    }
+    {
+        const auto item = strings.PopMax();
+        ASSERT_EQUAL(item.first, "yellow");
+        ASSERT_EQUAL(item.second, 2);
+    }
+    {
+        const auto item = strings.PopMax();
+        ASSERT_EQUAL(item.first, "white");
+        ASSERT_EQUAL(item.second, 0);
+    }
 }
 
 void TestAll()
 {
     TestRunner tr{};
-    RUN_TEST(tr, TestIntVector);
+    RUN_TEST(tr, TestNoCopy);
 }
+
+// class Logger
+// {
+// public:
+//     Logger() { cout << "Default ctor\n"; }
+//     Logger(const Logger &) { cout << "Copy ctor\n"; }
+//     void operator=(const Logger &) { cout << "Copy assignment ctor\n"; }
+//     Logger(Logger &&) { cout << "Move ctor\n"; }
+//     void operator=(Logger &&) { cout << "Move assignment ctor\n"; }
+// };
+
+// template <typename T>
+// void PushBack(T &&object)
+// {
+
+// }
 
 void Profile()
 {
+    // Logger log;
+
+    // PushBack(log);
 }
