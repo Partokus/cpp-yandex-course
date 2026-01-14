1e0034b1c19d625b3173a6e70680097ef94ac935
a.lepeshkin <a.lepeshkin@1440.space>
2026-01-14 17:07:50 +0300

    Point Rgb Color

--- src/main.cpp
+++ src/main.cpp
@@ -35,9 +35,107 @@ using namespace std::chrono_literals;
 void TestAll();
 void Profile();
 
+namespace Svg
+{
+
+struct Point
+{
+    double x = 0.0;
+    double y = 0.0;
+};
+
+struct Rgb
+{
+    uint8_t red = 0U;
+    uint8_t green = 0U;
+    uint8_t blue = 0U;
+};
+
+ostream & operator<<(ostream &os, const Rgb &v)
+{
+    return os << "rgb("    <<
+        to_string(v.red)   << ',' <<
+        to_string(v.green) << ',' <<
+        to_string(v.blue)  << ')';
+}
+
+struct Color
+{
+    Color() = default;
+    Color(const Rgb &v) : value(v) {}
+    Color(const string &v) : value(v) {}
+
+    variant<string, Rgb> value = "none"s;
+};
+
+ostream & operator<<(ostream &os, const Color &v)
+{
+    if (holds_alternative<string>(v.value))
+        return os << get<string>(v.value);
+    return os << get<Rgb>(v.value);
+}
+
+Color NoneColor{};
+
+}
+
+void TestRgbCout()
+{
+    using namespace Svg;
+    {
+        ostringstream oss;
+        oss << Rgb{255, 255, 255};
+        ASSERT_EQUAL(oss.str(), "rgb(255,255,255)");
+    }
+    {
+        ostringstream oss;
+        oss << Rgb{10, 50, 70};
+        ASSERT_EQUAL(oss.str(), "rgb(10,50,70)");
+    }
+    {
+        ostringstream oss;
+        Rgb rgb;
+        rgb.red = 10U;
+        rgb.green = 50U;
+        rgb.blue = 70U;
+        oss << rgb;
+        ASSERT_EQUAL(oss.str(), "rgb(10,50,70)");
+    }
+}
+
+void TestColorCout()
+{
+    using namespace Svg;
+    {
+        ostringstream oss;
+        Color color;
+        oss << color;
+        ASSERT_EQUAL(oss.str(), "none");
+    }
+    {
+        ostringstream oss;
+        oss << NoneColor;
+        ASSERT_EQUAL(oss.str(), "none");
+    }
+    {
+        ostringstream oss;
+        Color color{"RED"};
+        oss << color;
+        ASSERT_EQUAL(oss.str(), "RED");
+    }
+    {
+        ostringstream oss;
+        Color color{Rgb{10, 0, 150}};
+        oss << color;
+        ASSERT_EQUAL(oss.str(), "rgb(10,0,150)");
+    }
+}
+
 void TestAll()
 {
     TestRunner tr{};
+    RUN_TEST(tr, TestRgbCout);
+    RUN_TEST(tr, TestColorCout);
 }
 
 void Profile()
@@ -47,37 +145,6 @@ void Profile()
 int main()
 {
     TestAll();
-
-    int64_t left = 0;
-    int64_t right = 0;
-
-    cin >> left >> right;
-
-    if (left >= 0 and right >= 0)
-    {
-        uint64_t result = static_cast<uint64_t>(left) + static_cast<uint64_t>(right);
-        if (result > std::numeric_limits<int64_t>::max())
-            cout << "Overflow!";
-        else
-            cout << left + right << endl;
-    }
-    else if (left <= 0 and right <= 0)
-    {
-        uint64_t u_left = left == std::numeric_limits<int64_t>::min() ? static_cast<uint64_t>(left)
-                                                                      : left * (-1);
-        uint64_t u_right = right == std::numeric_limits<int64_t>::min() ? static_cast<uint64_t>(right)
-                                                                        : right * (-1);
-        uint64_t result = u_left + u_right;
-        
-        if (result > static_cast<uint64_t>( std::numeric_limits<int64_t>::min() ))
-            cout << "Overflow!";
-        else
-            cout << left + right << endl;
-    }
-    else
-    {
-        cout << left + right << endl;
-    }
 
     return 0;
 }
