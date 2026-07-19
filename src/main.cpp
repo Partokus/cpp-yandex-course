938e30c7f490451d88c9145b4964ea294247f62a
partokus <lepeshkin-av-97@mail.ru>
2026-07-19 18:28:04 +0300

    prepare new task

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,12 @@
 #include <profile.h>
 #include <test_runner.h>
 
-#include "trans.h"
-#include "trans_test.h"
-#include "json.h"
-#include "router.h"
-#include "svg.h"
-
 void TestAll();
 void Profile();
 
 void TestAll()
 {
     TestRunner tr{};
-    RUN_TEST(tr, TestParseJson);
-    RUN_TEST(tr, TestParseAddStopQuery);
-    RUN_TEST(tr, TestParseAddBusQuery);
-    RUN_TEST(tr, TestCalcGeoDistance);
-    RUN_TEST(tr, TestDataBaseCreateInfo);
-    RUN_TEST(tr, TestDataBaseCreateGraph);
-    RUN_TEST(tr, TestBuildRoute);
-    RUN_TEST(tr, TestParseRouteQuery);
-    RUN_TEST(tr, TestParse);
-
-    RUN_TEST(tr, TestRgbCout);
-    RUN_TEST(tr, TestColorCout);
-    RUN_TEST(tr, TestDocument);
-    RUN_TEST(tr, TestMakeRenderSettigs);
-    RUN_TEST(tr, Test15);
-    RUN_TEST(tr, TestCreateMap);
-    RUN_TEST(tr, TestRender0);
-    RUN_TEST(tr, TestRender1);
-    RUN_TEST(tr, TestRender2);
-    RUN_TEST(tr, TestRenderBusNames);
 }
 
 void Profile()
@@ -45,7 +19,5 @@ int main()
     TestAll();
 #endif
 
-    DataBase db;
-    Parse(cin, cout, db);
     return 0;
 }
--- src/render.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "render_types.h"
-#include "json.h"
-#include "trans_data_base.h"
-
-RenderSettings MakeRenderSettigs(const std::map<std::string, Json::Node> &render_settings);
-
-std::string CreateMap(DataBase &db);
